05068ee15defe80c94b2a1219144f0ff613062ab
Dionatan Ribeiro <dionatangil@gmail.com>
2019-10-10 22:04:28 -0300

    adicionar id para usuario

--- src/main/java/br/com/cwi/cursoaws/service/UserServiceImpl.java
+++ src/main/java/br/com/cwi/cursoaws/service/UserServiceImpl.java
@@ -12,11 +12,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return Arrays.asList(
-                User.builder().name("Enrique Thompson").age(23).build(),
-                User.builder().name("Eric Duncan").age(26).build(),
-                User.builder().name("Tanya Sutton").age(24).build(),
-                User.builder().name("Isabel Fisher").age(21).build(),
-                User.builder().name("Brandon Jones").age(25).build()
+                User.builder().id(1L).name("Enrique Thompson").age(23).build(),
+                User.builder().id(2L).name("Eric Duncan").age(26).build(),
+                User.builder().id(3L).name("Tanya Sutton").age(24).build(),
+                User.builder().id(4L).name("Isabel Fisher").age(21).build(),
+                User.builder().id(5L).name("Brandon Jones").age(25).build()
         );
     }
 }
